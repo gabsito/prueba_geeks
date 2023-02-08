95e7c9874a379854f0ff76b4777777c68a223605
gabsito <gacagonz@espol.edu.ec>
2023-02-08 10:42:21 -0500

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TheHeaderComponent } from './the-header/the-header.component';
+import { HeroComponent } from './hero/hero.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RatesComponent } from './rates/rates.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { TradeComponent } from './trade/trade.component';
+import { BlogComponent } from './blog/blog.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { DownloadAppComponent } from './download-app/download-app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheHeaderComponent,
+    HeroComponent,
+    WelcomeComponent,
+    RatesComponent,
+    ExperienceComponent,
+    TradeComponent,
+    BlogComponent,
+    TestimonialsComponent,
+    DownloadAppComponent,
+    FooterComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
